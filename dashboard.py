bf7a2e1a3ab504cc3d3b6be91ce4deb5ca59375b
JuanGalicia20 <69065800+JuanGalicia20@users.noreply.github.com>
2025-05-13 20:07:23 -0600

    Modified libraries

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import mysql.connector
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
